078510a929cc47eb018013790a43a9cfc080310a
Rangga Eka Kusuma Dani <ranggaeka165@gmail.com>
2024-01-04 11:27:16 +0700

    Tugas 13 (Fix Error XML)

--- LumenServiceApp/app/Http/Controllers/PublicController/PostsController.php
+++ LumenServiceApp/app/Http/Controllers/PublicController/PostsController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\PublicController;
 
@@ -6,10 +6,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-class PostsController extends Controller{
-    public function index(Request $request){
+class PostsController extends Controller
+{
+    public function index(Request $request)
+    {
+        // print_r($request->all());die();
         $posts = Post::OrderBy("id", "ASC")->paginate(10)->toArray();
-        $acceptHeader = $request->header('Accept');
         $response = [
             "total_count" => $posts["total"],
             "limit" => $posts["per_page"],
@@ -19,15 +21,34 @@ class PostsController extends Controller{
             ],
             "data" => $posts["data"],
         ];
-        return response()->json($response,200);
+        $acceptHeader = $request->header('Accept');
+        if ($acceptHeader === 'application/json') {
+            return response()->json($response, 200);
+        } else {
+            $xml = new \SimpleXMLElement('<Posts/>');
+            foreach ($posts['data'] as $item) {
+                $xmlItem = $xml->addChild('post');
+                $xmlItem->addChild('id', $item['id']);
+                $xmlItem->addChild('title', $item['title']);
+                $xmlItem->addChild('author', $item['author']);
+                $xmlItem->addChild('category', $item['category']);
+                $xmlItem->addChild('status', $item['status']);
+                $xmlItem->addChild('content', $item['content']);
+                $xmlItem->addChild('user_id', $item['user_id']);
+                $xmlItem->addChild('created_at', $item['created_at']);
+                $xmlItem->addChild('updated_at', $item['updated_at']);
+            }
+            return $xml->asXML();
+        }
     }
 
-    public function show($id){
-        $post = Post::with(['user'=>function($query){
-            $query->select('id','name');
+    public function show($id)
+    {
+        $post = Post::with(['user' => function ($query) {
+            $query->select('id', 'name');
         }])->find($id);
-        
-        if(!$post){
+
+        if (!$post) {
             abort(404);
         }
         return response()->json([
